cb94b76d6c75755667a1e013b5eb0bf39ea6eb99
kareinonitsuke <>
2021-03-12 22:32:46 +0900

    [clean] 関数をモジュールに分割しました(2)

---
+++ src/common.rs
@@ -0,0 +1,34 @@
+pub mod post{
+    pub async fn post_message(ctx: &crate::Context, msg: &crate::Message, message_str: String) {
+        let response = crate::MessageBuilder::new()
+        .push(message_str)
+        .build();
+        if let Err(why) = msg.channel_id.say(&ctx.http, &response).await {
+            println!("Error sending message: {:?}", why);
+        }
+    }
+}
+
+pub mod channel{
+    pub async fn get_channel_name(ctx: &crate::Context, msg: &crate::Message) -> String {
+        let channel_name = match msg.channel_id.to_channel(&ctx).await{
+            Ok(channel) =>channel,
+            Err(why) =>{
+                println!("Error:{:?}",why);
+                return "".to_string();
+            },
+        };
+        return channel_name.to_string();
+    }
+    
+    pub async fn is_target_channel(channel_name: String) -> bool{
+        let target_channel = crate::env::var("TARGET_CHANNEL")
+            .expect("Expected a target channnel");
+        if channel_name != target_channel {
+            println!("This is not target channel:{}",channel_name);
+            return false;
+        }
+        return true;
+    }
+    
+}
---
+++ src/role.rs
@@ -0,0 +1,28 @@
+pub async fn role_setting(ctx: &crate::Context, msg: &crate::Message, role_string: &String) -> bool{
+    let add_role_command = "!role-i-want-an-announcement";
+    let remove_role_command = "!role-i-don't-want-an-announcement";
+    let role = "RM-announce";
+    
+    println!("command is:{}", role_string);
+    
+    if role_string.starts_with(add_role_command){
+        crate::common::post::post_message(&ctx,&msg,"roleふってあげたいね".to_string()).await;
+    }
+    else if role_string.starts_with(remove_role_command){
+        crate::common::post::post_message(&ctx,&msg,"role外してあげたいね".to_string()).await;
+    }
+    return true;
+}
+
+async fn have_a_role() -> bool {
+    return false;
+}
+
+async fn add_role() -> bool {
+    return false;
+}
+
+async fn remove_role() -> bool {
+    return false;
+}
+
