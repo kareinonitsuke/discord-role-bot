d252363a9556b080ff1ab5e110c8fea54eb0c632
kareinonitsuke <>
2021-03-12 22:31:12 +0900

    [clean] 関数をモジュールに分割しました

--- src/main.rs
+++ src/main.rs
@@ -14,81 +14,26 @@ enum TypeOfCommand{
     RMeeting(String),
 }
 
+pub mod role;
+pub mod common;
+//use crate::common::{post, channel};
+
 #[macro_use]
 extern crate scan_fmt;
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        let channel_name = get_channel_name(&ctx,&msg).await;
+        let channel_name = common::channel::get_channel_name(&ctx,&msg).await;
         println!("channelIs:{}", channel_name);
-        if is_target_channel(channel_name).await {
-            role_setting(&ctx,&msg,&msg.content).await;
+        if common::channel::is_target_channel(channel_name).await {
+            role::role_setting(&ctx,&msg,&msg.content).await;
         }
     }
 
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
-}
-
-
-async fn role_setting(ctx: &Context, msg: &Message, role_string: &String) -> bool{
-    let add_role_command = "!role-i-want-an-announcement";
-    let remove_role_command = "!role-i-don't-want-an-announcement";
-    let role = "RM-announce";
-    
-    println!("command is:{}", role_string);
-    
-    if role_string.starts_with(add_role_command){
-        post_message(&ctx,&msg,"roleふってあげたいね".to_string()).await;
-    }
-    else if role_string.starts_with(remove_role_command){
-        post_message(&ctx,&msg,"role外してあげたいね".to_string()).await;
-    }
-    return true;
-}
-
-async fn have_a_role() -> bool {
-    return false;
-}
-
-async fn add_role() -> bool {
-    return false;
-}
-
-async fn remove_role() -> bool {
-    return false;
-}
-
-async fn post_message(ctx: &Context, msg: &Message, message_str: String) {
-    let response = MessageBuilder::new()
-    .push(message_str)
-    .build();
-    if let Err(why) = msg.channel_id.say(&ctx.http, &response).await {
-        println!("Error sending message: {:?}", why);
-    }
-}
-
-async fn get_channel_name(ctx: &Context, msg: &Message) -> String {
-    let channel_name = match msg.channel_id.to_channel(&ctx).await{
-        Ok(channel) =>channel,
-        Err(why) =>{
-            println!("Error:{:?}",why);
-            return "".to_string();
-        },
-    };
-    return channel_name.to_string();
-}
-
-async fn is_target_channel(channel_name: String) -> bool{
-    let target_channel = env::var("TARGET_CHANNEL")
-        .expect("Expected a target channnel");
-    if channel_name != target_channel {
-        println!("This is not target channel:{}",channel_name);
-        return false;
-    }
-    return true;
 }
 
 #[tokio::main]
